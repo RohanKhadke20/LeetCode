4ee11c553a4058cdd061da52f5e8479c1e69db53
Rohan Chandrakant Khadke <khadkerohan20@gmail.com>
2026-07-04 20:44:56 +0530

    Time: 0 ms (100%), Space: 9 MB (9.08%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int l = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s[i] != ' ') {
-                l++;
-            } else if (l > 0) {
-                break;
-            }
+       int i = s.size() - 1;
+
+        while (i >= 0 && s[i] == ' ') {
+            i--;
         }
+
+        int l = 0;
+
+        while (i >= 0 && s[i] != ' ') {
+            l++;
+            i--;
+        }
+
         return l;
     }
 };
